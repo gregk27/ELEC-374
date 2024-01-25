8e769d4c802503c597d75aec261821b4aa9983ce
gregk27 <git@gregk.ca>
2024-01-25 09:42:57 -0500

    Update datapath with more devices

--- DataPath.v
+++ DataPath.v
@@ -1,24 +1,53 @@
 module DataPath(
 	input wire clock, clear,
-	input wire [7:0] A, 
-	input wire [7:0] RegisterAImmediate,
-	input wire RZout, RAout, RBout,
-	input wire RAin, RBin, RZin
+	input wire R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in, PCin, IRin, RYin, RZin, MARin, RHIin, RLOin
 );
 
-wire [7:0] BusMuxOut, BusMuxInRZ, BusMuxInRA, BusMuxInRB; 
+wire [31:0] BusMuxInR0, BusMuxInR1, BusMuxInR2, BusMuxInR3, BusMuxInR4, BusMuxInR5, BusMuxInR6, BusMuxInR7, BusMuxInR8, BusMuxInR9, BusMuxInR10, BusMuxInR11, BusMuxInR12, BusMuxInR13, BusMuxInR14, BusMuxInR15, BusMuxInPC, BusMuxInIR, BusMuxInRY, BusMuxInRZ, BusMuxInMAR, BusMuxInRHI, BusMuxInRLO;
 
-wire [7:0] Zregin;
 
 //Devices
-register RA(clear, clock, RAin, RegisterAImmediate, BusMuxInRA);
-register RB(clear, clock, RBin, BusMuxOut, BusMuxInRB);
 
-// adder
-adder add(A, BusMuxOut, Zregin);
-register RZ(clear, clock, RZin, Zregin, BusMuxInRZ);
+
+// Registers
+register R0(clear, clock, R0in, BusMuxOut, BusMuxInR0);
+register R1(clear, clock, R1in, BusMuxOut, BusMuxInR1);
+register R2(clear, clock, R2in, BusMuxOut, BusMuxInR2);
+register R3(clear, clock, R3in, BusMuxOut, BusMuxInR3);
+register R4(clear, clock, R4in, BusMuxOut, BusMuxInR4);
+register R5(clear, clock, R5in, BusMuxOut, BusMuxInR5);
+register R6(clear, clock, R6in, BusMuxOut, BusMuxInR6);
+register R7(clear, clock, R7in, BusMuxOut, BusMuxInR7);
+register R8(clear, clock, R8in, BusMuxOut, BusMuxInR8);
+register R9(clear, clock, R9in, BusMuxOut, BusMuxInR9);
+register R10(clear, clock, R10in, BusMuxOut, BusMuxInR10);
+register R11(clear, clock, R11in, BusMuxOut, BusMuxInR11);
+register R12(clear, clock, R12in, BusMuxOut, BusMuxInR12);
+register R13(clear, clock, R13in, BusMuxOut, BusMuxInR13);
+register R14(clear, clock, R14in, BusMuxOut, BusMuxInR14);
+register R15(clear, clock, R15in, BusMuxOut, BusMuxInR15);
+
+// Control
+register PC(clear, clock, PCin, BusMuxOut, BusMuxInPC);
+register IR(clear, clock, IRin, BusMuxOut, BusMuxInIR);
+
+// Memory
+register MAR(clear, clock, MARin, BusMuxOut, BusMuxInMAR);
+
+// ALU
+register RY(clear, clock, RYin, BusMuxOut, BusMuxInRY);
+register RZ(clear, clock, RZin, BusMuxOut, BusMuxInRZ);
+register RHI(clear, clock, RHIin, BusMuxOut, BusMuxInRHI);
+register RLO(clear, clock, RLOin, BusMuxOut, BusMuxInRLO);
+
 
 //Bus
-Bus bus(BusMuxInRZ, BusMuxInRA, BusMuxInRB, RZout, RAout, RBout, BusMuxOut);
+Bus bus(
+	// Data In
+	BusMuxInR0, BusMuxInR1, BusMuxInR2, BusMuxInR3, BusMuxInR4, BusMuxInR5, BusMuxInR6, BusMuxInR7, BusMuxInR8, BusMuxInR9, BusMuxInR10, BusMuxInR11, BusMuxInR12, BusMuxInR13, BusMuxInR14, BusMuxInR15, BusMuxInPC, BusMuxInIR, BusMuxInRY, BusMuxInRZ, BusMuxInMAR, BusMuxInRHI, BusMuxInRLO, 
+	// Select signals
+	R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in, PCin, IRin, RYin, RZin, MARin, RHIin, RLOin,
+	// Out	
+	BusMuxOut);
 
 endmodule
