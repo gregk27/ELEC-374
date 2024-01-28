8f98024819c4bc4960a1eec13b01db7e0f0da42c
gregk27 <git@gregk.ca>
2024-01-28 14:43:39 -0500

    Reimplement adder as Carry-lookahead

--- adder.v
+++ adder.v
@@ -1,21 +1,25 @@
-// Ripple Carry Adder
-module adder(A, B, Result);
+// Carry-Lookahead Adder
+module adder(input [31:0]A, input [31:0]B, output [31:0] Result);
 
-input [31:0] A, B;
-output [31:0] Result;
+reg [31:0] Results;
+reg [31:0] Generate;
+reg [32:0] Propogate;
+reg [33:0] LocalCarry;
 
-reg [31:0] Result;
-reg [32:0] LocalCarry;
+assign Result = Results;
 
 integer i;
 
 always@(A or B)
 	begin
 		LocalCarry = 32'd0;
+		// Perform these operations first as they can run in parallel
+		Generate = A & B;
+		Propogate = A | B;
 		for(i = 0; i < 32; i = i + 1)
 		begin
-				Result[i] = A[i]^B[i]^LocalCarry[i];
-				LocalCarry[i+1] = (A[i]&B[i])|(LocalCarry[i]&(A[i]|B[i]));
+				LocalCarry[i+1] = (Generate[i] | (Propogate[i] & LocalCarry[i]));
+				Results[i] = A[i] ^ B[i] ^ LocalCarry[i];
 		end
 end
 endmodule
