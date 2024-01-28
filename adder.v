b6b50a304037fc7f3bbb9c5ff6ebf66b736dc6b7
gregk27 <git@gregk.ca>
2024-01-28 18:08:53 -0500

    Update adder to support subtraction
    Also update testbench accordingly

--- adder.v
+++ adder.v
@@ -1,25 +1,30 @@
 // Carry-Lookahead Adder
-module adder(input [31:0]A, input [31:0]B, output [31:0] Result);
+module adder(input [31:0]A, input [31:0]B, input subtract, output [31:0]Result);
 
 reg [31:0] Results;
 reg [31:0] Generate;
 reg [32:0] Propogate;
 reg [33:0] LocalCarry;
 
+reg [31:0] Btmp;
+
 assign Result = Results;
 
 integer i;
 
-always@(A or B)
+always@(A or B or subtract)
 	begin
-		LocalCarry = 32'd0;
+		// Carry in first bit based on subtract flag
+		LocalCarry = 32'd0 | subtract;
+		// Negate B based on subtract bit
+		Btmp = B ^ {32{subtract}};
 		// Perform these operations first as they can run in parallel
-		Generate = A & B;
-		Propogate = A | B;
+		Generate = A & Btmp;
+		Propogate = A | Btmp;
 		for(i = 0; i < 32; i = i + 1)
 		begin
 				LocalCarry[i+1] = (Generate[i] | (Propogate[i] & LocalCarry[i]));
-				Results[i] = A[i] ^ B[i] ^ LocalCarry[i];
+				Results[i] = A[i] ^ Btmp[i] ^ LocalCarry[i];
 		end
 end
 endmodule
--- adder_tb.v
+++ adder_tb.v
@@ -5,12 +5,13 @@ reg clock;
 reg [3:0] present_state;
 
 reg [31:0]a, b;
+reg subtract;
 wire [31:0]result;
 
 reg [31:0] expectedResult;
 
 adder add(
-	a, b, result
+	a, b, subtract, result
 );
 
 parameter init = 4'd1, T0 = 4'd2, T1 = 4'd3, T2 = 4'd4;
@@ -23,19 +24,23 @@ always @(present_state) begin
 	case(present_state)
 		init: begin
 			a <= 0; b <= 0;
-			expectedResult <= 0;
+			#0 subtract <= 0; expectedResult <= 0;
+			#10 subtract <= 1; expectedResult <= 0;
 		end
 		T0: begin
 			a <= 25; b <= 40;
-			expectedResult <= 65;
+			#0 subtract <= 0; expectedResult <= 65;
+			#10 subtract <= 1; expectedResult <= -15;
 		end
 		T1: begin
 			a <= -50; b <= -10;
-			expectedResult <= -60;
+			#0 subtract <= 0; expectedResult <= -60;
+			#10 subtract <= 1; expectedResult <= -40;
 		end
 		T2: begin
 			a <= -12; b <= 25;
-			expectedResult <= 13;
+			#0 subtract <= 0; expectedResult <= 13;
+			#10 subtract <= 1; expectedResult <= -37;
 		end
 	endcase
 end
