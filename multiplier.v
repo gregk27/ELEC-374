6933dd697e54d8bf39695b27ffb03fdad0fb2eac
ZanePrance <zane550prance@gmail.com>
2024-02-01 10:50:05 -0500

    bit pair recoding

--- multiplier.v
+++ multiplier.v
@@ -3,38 +3,43 @@ module multiplier(multiplicand, multiplier, product_lo, product_hi);
 input [31:0] multiplicand, multiplier;
 output [31:0] product_lo, product_hi;
 
-reg[31: 0] acc, product_hi;
-reg[32:0] extended_multiplier;
+reg [63:0] product; // Use a single product register to simplify operations
+reg [31:0] product_hi, acc;
+reg [32:0] extended_multiplier;
 integer i;
 
 always @(multiplicand or multiplier) begin
     acc = 32'd0;
     product_hi = 32'd0;
-    extended_multiplier = {multiplier, 1'b0};
+    extended_multiplier = {multiplier, 1'b0}; // Extend multiplier by 1 bit for bit-pair recoding
 
-    for(i = 0; i < 32; i = i + 1)
-    begin
-        case (extended_multiplier[1:0])
-            2'b01:
-            begin
-                {product_hi, acc} = {product_hi, acc} + {32'b0, multiplicand}; //might have to send to the adder
+    for(i = 0; i < 32; i = i + 1) begin
+        case (extended_multiplier[2:0])
+            3'b001, 3'b010: begin
+                product = product + {32'b0, multiplicand}; // +1
             end
-            2'b10:
-            begin
-                {product_hi, acc} = {product_hi, acc} - {32'b0, multiplicand}; //might have to send to the adder
+            3'b011: begin
+                product = product + {31'b0, multiplicand, 1'b0}; // +2
+            end
+            3'b100: begin
+                product = product - {31'b0, multiplicand, 1'b0}; // -2
+            end
+            3'b101, 3'b110: begin
+                product = product - {32'b0, multiplicand}; // -1
             end
             
-            default: ; //Do nothing for 00 or 11
+            default: ; // Do nothing for 000, 111
         endcase
-        //arithmetic right shift to shift the accumulater reg and extended multiplier
-        //maintains the sign of the 2 bit complement
-        {product_hi, acc, extended_multiplier} = {product_hi[31], product_hi, acc, extended_multiplier} >> 1;
-
+        // maintain sign
+        product = {product[63], product} >> 1;
+        extended_multiplier = extended_multiplier >> 1; // Align extended_multiplier for next iteration
     end
     
+    product_hi = product[63:32];
+    acc = product[31:0];
 end
-assign product_lo = acc;//Output the lower 32 bits
 
-//the upper 32 bits remains in product_hi register outside of the module
-//the value can be accessed after the multiplication is complete
+assign product_lo = acc; // Output the lower 32 bits
+assign product_hi = product_hi; // Output the upper 32 bits
+
 endmodule
