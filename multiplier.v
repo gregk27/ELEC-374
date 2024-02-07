79811ebe0d537963fe7d25bec4608e5445ebfb36
ZanePrance <141082203+ZanePrance@users.noreply.github.com>
2024-02-06 19:35:07 -0500

    Update multiplier.v

--- multiplier.v
+++ multiplier.v
@@ -1,5 +1,5 @@
 module multiplier(
-    input wire clk,
+    input wire clock,
     input wire start, 
     input wire [31:0] multiplicand,
     input wire [31:0] multiplier,
@@ -13,22 +13,18 @@ reg [32:0] extended_multiplier;
 reg [5:0] count;
 
 
+initial finished <= 0;
 
-initial begin
-    finished <= 0;
-end
-
-always @(posedge clk) begin
-    if(start) begin
-        //intilize the product and extended multiplier
-    product <= 64'd0; 
-	extended_multiplier <= {multiplier, 1'b0}; 
-    count <= 6'd0;
-    finished <= 0;
-
-    end
-    else if(count < 32) begin
-        case (extended_multiplier[2:0])
+always @(posedge clock, posedge start) begin
+	if(start) begin
+		//intilize the product and extended multiplier
+   		product <= 64'd0; 
+		extended_multiplier <= {multiplier, 1'b0}; 
+    	count <= 6'd0;
+		count <= 0;
+		finished <= 0;
+	end else if(count != 32) begin
+			case (extended_multiplier[2:0])
 			3'b001, 3'b010: product = product + ({32'b0, multiplicand} << count);
             3'b011: product = product + ({32'b0, multiplicand} << (count + 1));
             3'b100: product = product - ({32'b0, multiplicand} << (count + 1));
@@ -38,17 +34,14 @@ always @(posedge clk) begin
         endcase
         extended_multiplier = extended_multiplier >> 2;
         count = count + 2;
-    end
-    else begin
-        finished <= 1;
-    end
+	end else if (count >= 32 && !finished) begin
+		finished <= 1;
+	end
+	
 end
 
-// Output assignments
-assign product_lo = product[31:0];
-assign product_hi = product[63:32];
+assign product_lo <= product[31:0];
+assign product_hi <=product[63:32];
+
 
 endmodule
-            
-
-			
