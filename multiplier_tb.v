ba7100d1fdbd8833289cc882025b2c5a5d8ebfcb
ZanePrance <141082203+ZanePrance@users.noreply.github.com>
2024-02-06 19:35:28 -0500

    Update multiplier_tb.v

--- multiplier_tb.v
+++ multiplier_tb.v
@@ -1,47 +1,64 @@
 `timescale 1ns/10ps
-
 module multiplier_tb();
 
 reg clock;
+reg [3:0] present_state;
+
 reg start;
-reg [31:0] multiplicand;
-reg [31:0] multiplier;
-wire [31:0] product_low;
-wire [31:0] product_high;
+reg [31:0]multiplicand;
+reg [31:0]multiplier;
+wire [31:0]product_lo;
+wire [31:0]product_hi;
 wire finished;
 
-reg [63:0] expectedProduct;
+reg [31:0] expectedProduct;
 
-multiplier mult(
-    .clk(clock),
-    .start(start),
-    .multiplicand(multiplicand),
-    .product_lo(product_low),
-    .product_hi(product_high),
-    .finished(finished)
+
+multiplier mul(
+	clock,
+	start,
+	multiplicand,
+	multiplier,
+	product_lo,
+	product_hi,
+	finished
 );
 
-initial begin
-    clock = 0;
-    forever #5 clock = ~clock;
-    end
 
-initial begin
-    start = 0;
-    multiplicand = 0;
-    multiplier = 0;
-    #10;
-    // test 1
-    multiplicand = 32'd15;
-    multiplier = 32'd15;
-    expectedProduct = 64'd225;
-    #10;
-    start = 1;
-    #10;
-    start = 0;
-    wait(finished);
-    
-    $finish;
+parameter init = 4'd0, T0 = 4'd1, T1 = 4'd2, T2 = 4'd3;
+			 
+initial begin clock = 0; present_state = 4'd0; end
+always #5 clock = ~clock;
+// Tick over when the divider reports finished
+always @ (posedge finished) #15 present_state = present_state + 1;
+
+
+always @(present_state) begin
+	case(present_state)
+		init: begin
+			multiplier <= 4; multiplicand <= 2;
+			expectedProduct <= 8; 
+			#0 start <= 1;
+			#5 start <= 0;
+		end
+		T0: begin
+			multiplier <= 27; multiplicand <= -8;
+			expectedProduct <= -216; 
+			#0 start <= 1;
+			#5 start <= 0;
+		end
+		T1: begin
+			multiplier <= -14; multiplicand <= 4;
+			expectedProduct <= -56; 
+			#0 start <= 1;
+			#5 start <= 0;
+		end
+		T2: begin
+			multiplier <= -10; multiplicand <= -8;
+			expectedProduct <= 80; 
+			#0 start <= 1;
+			#5 start <= 0;
+		end
+	endcase
 end
-
 endmodule
