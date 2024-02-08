384f26f520da8143978898723bb15d44981f3545
Gregory Kelly <git@gregk.ca>
2024-02-07 23:31:11 -0500

    Fix bugs with negative multiplicand

--- multiplier.v
+++ multiplier.v
@@ -12,6 +12,7 @@ reg [63:0] product;
 reg [32:0] extended_multiplier;
 reg [5:0] count;
 
+reg [31:0] multiplicand_pos;
 
 initial finished <= 0;
 
@@ -19,22 +20,25 @@ always @(posedge clock, posedge start) begin
 	if(start) begin
 		//intilize the product and extended multiplier
    		product <= 64'd0; 
-		extended_multiplier <= {multiplier, 1'b0}; 
+		extended_multiplier <= {multiplier, 1'b0};
+		multiplicand_pos <= multiplicand[31] ? -multiplicand : multiplicand;
     	count <= 6'd0;
 		count <= 0;
 		finished <= 0;
 	end else if(count != 32) begin
 		case (extended_multiplier[2:0])
-			3'b001, 3'b010: product = product + ({32'b0, multiplicand} << count);
-            	3'b011: product = product + ({32'b0, multiplicand} << (count + 1));
-            	3'b100: product = product - ({32'b0, multiplicand} << (count + 1));
-            	3'b101, 3'b110: product = product - ({32'b0, multiplicand} << count);
+			3'b001, 3'b010: product = product + ({32'b0, multiplicand_pos} << count);
+            	3'b011: product = product + ({32'b0, multiplicand_pos} << (count + 1));
+            	3'b100: product = product - ({32'b0, multiplicand_pos} << (count + 1));
+            	3'b101, 3'b110: product = product - ({32'b0, multiplicand_pos} << count);
 
             	default: ;
         endcase
         	extended_multiplier = extended_multiplier >> 2;
         	count = count + 2;
 	end else if (count >= 32 && !finished) begin
+		if (multiplicand[31])
+			product <= -product;
 		finished <= 1;
 	end
 	
