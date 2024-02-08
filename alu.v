bcdc6242867ae6fbdb3cd9563e6e5324082098cb
gregk27 <git@gregk.ca>
2024-02-07 20:51:11 -0500

    Update ALU to support adder
    Creates adder module, also includes some structural changes to improve
    behaviour

--- alu.v
+++ alu.v
@@ -26,13 +26,56 @@ parameter
 	ROR 	= 5'b11011,
 	SHRA 	= 5'b11100,
 	SHLA 	= 5'b11101;
+
+// Inernal status register used to indicate setup phase
+reg setup;
+
+
+reg subtract;
+reg [31:0] adder_out;
+reg [31:0] negate_mux;
+adder add(negate_mux, B, subtract, adder_out);
+
 	
 always @(A, B, opSelect) begin
 	finished <= 0;
-	case (opSelect)
-		NOT: out <= ~A; finished <= 1;
-		AND: out <= A&B; finished <= 1;
-		OR : out <= A|B; finished <= 1;
-	endcase
+	setup <= 1;
 end
+
+// Run on negedge clock to have values ready by the positive edge
+always @(negedge clock) begin
+	if(setup) begin
+		setup <= 0;
+		// First run setup to configure the inputs and outputs to perform the calculation
+		case (opSelect)
+			NOT:  begin out <= ~A; finished <= 1; end
+			AND:  begin out <= A&B; finished <= 1; end
+			OR : begin out <= A|B; finished <= 1; end
+			ADD, SUB, NEG: begin
+				// Subtract based on bit 0
+				subtract <= opSelect[0];
+				// Mux in a 0 if negate bit is high
+				negate_mux <= opSelect[1] ? 0 : A;
+			end
+		endcase
+	end
+	else begin
+		// Once setup is complete, monitor outputs for completion (if applicable)
+		case (opSelect)
+			ADD, SUB, NEG: begin
+				// Adder runs in 1 cycle, so always ready
+				out <= adder_out;
+				finished <= 1;
+			end
+		endcase
+	end
+end
+
+// Update output and finished as the data becomes available
+always @(negedge clock) begin
+	if(!setup) begin
+
+	end
+end
+
 endmodule
